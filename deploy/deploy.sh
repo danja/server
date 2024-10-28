cbc4ba2870066cb190f4230c9a5eb4c60d41d144
Danny Ayers <danny.ayers@gmail.com>
2024-10-28 22:11:05 +0100

    ee too many bits

---
+++ deploy/deploy.sh
@@ -0,0 +1,11 @@
+cd ~/github-danny/transmissions
+
+./trans postcraft ../postcraft/danny.ayers.name
+
+./trans postcraft ../transmissions/docs/postcraft
+
+./trans postcraft ../hyperdata/packages/farelo/docs/postcraft
+
+./trans ../trans-apps/applications/git-apps
+
+#github-list-transmission.ttl
