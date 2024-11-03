dd6cce8f36a9b0cd2d167f0d2cb5d66ab6c74f8c
Danny Ayers <danny.ayers@gmail.com>
2024-11-03 14:48:37 +0100

    bits

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -2,7 +2,7 @@ cd ~/github-danny/transmissions
 
 ./trans postcraft ../server/docs/postcraft # typical
 ./trans postcraft docs/postcraft # for transmissions, it's already in the root dir
-./trans postcraft ../hyperdata/docs/postcraft # contains the partial
+# ./trans postcraft ../hyperdata/docs/postcraft # contains the partial
 
 # ./trans postcraft ../postcraft/danny.ayers.name
 # ./trans postcraft ../hyperdata/packages/farelo/docs/postcraft
